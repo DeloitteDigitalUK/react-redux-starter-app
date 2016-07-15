959adc5909d84a2c4de02f7016520310255a9571
O'Neill, Aiden (UK - Belfast) <aioneill@deloitte.co.uk>
2016-07-15 15:38:21 +0100

    Tweeter added (working)

--- src/components/Tweeter/TweetPage.js
+++ src/components/Tweeter/TweetPage.js
@@ -1,7 +1,6 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {browserHistory} from 'react-router';
 
 import TweetList from './TweetList';
 import * as tweetActions from './TweeterActions'
