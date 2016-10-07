af2deb106a7779de50d6d0818451e1567a1194d4
Vitaly Kondratiev <vitalkond@gmail.com>
2016-10-07 13:40:30 +0100

    removes typings

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/chai/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/whatwg-fetch/index.d.ts" />
-/// <reference path="globals/whatwg-streams/index.d.ts" />
